ced94ac6e8aa7ce5a8ff9bab23825059e01bc9aa
sehgol <>
2020-03-04 13:46:19 -0500

    signalKit updates

---
+++ SignalKit/SignalKit/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  SignalKit
+//  Created by Bilal Sehgol on 3/4/20.
+//  Copyright © 2020 BilalSehgol. All rights reserved.
+//
+
+import SwiftUI
+
+struct Signal {
+    // Range: 0 - 10
+    var intValue: Int
+    // Range: 0 - 1.0
+    var floatValue: Float
+    // True or False
+    var toggleValue: Bool
+}
+
+
+struct ContentView: View {
+ 
+    @State var signal = Signal(intValue: 0, floatValue: 0, toggleValue: false)
+    var body: some View {
+        VStack {
+            Controller(signal: $signal)
+            Spacer()
+            Visualizer(signal: signal)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SignalKit/SignalKit/Controller.swift
@@ -0,0 +1,43 @@
+//
+//  Controller.swift
+//  SignalKit
+//  Created by Bilal Sehgol on 3/4/20.
+//  Copyright © 2020 BilalSehgol. All rights reserved.
+//
+
+import SwiftUI
+
+struct Controller: View {
+    // Binding variable.
+    @Binding var signal: Double
+
+    var body: some View {
+
+        VStack{
+            
+                    Circle()
+                        .fill(Color.red)
+                        .blur(radius: CGFloat(signal), opaque: false)
+                        .transformEffect(CGAffineTransform(rotationAngle: CGFloat(.pi*signal)))
+
+                    Circle()
+                        .fill(Color.yellow)
+                        .blur(radius: CGFloat(signal+30), opaque: false)
+                        .transformEffect(CGAffineTransform(rotationAngle: CGFloat(.pi*signal/3600)))
+                        
+                    Circle()
+                       .fill(Color.green)
+                        .blur(radius: CGFloat(signal+60), opaque: false)
+                        .transformEffect(CGAffineTransform(rotationAngle: CGFloat(.pi*signal/21600)))
+
+        }
+            
+    }
+}
+
+struct Controller_Previews: PreviewProvider {
+    static var previews: some View {
+      Controller(signal: .constant(80))
+
+    }
+}
